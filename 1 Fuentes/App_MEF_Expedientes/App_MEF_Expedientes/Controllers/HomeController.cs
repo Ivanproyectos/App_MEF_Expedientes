9e232842965fba3b4be6a0a165837630a1895f41
Ivanproyectos <66085756+Ivanproyectos@users.noreply.github.com>
2021-10-27 23:38:09 -0500

    update

--- 1 Fuentes/App_MEF_Expedientes/App_MEF_Expedientes/Controllers/HomeController.cs
+++ 1 Fuentes/App_MEF_Expedientes/App_MEF_Expedientes/Controllers/HomeController.cs
@@ -9,25 +9,13 @@ namespace App_MEF_Expedientes.Controllers
 {
     public class HomeController : Controller
     {
-        public ActionResult Index()
+        public ActionResult Index(string usuario)
         {
-            ViewBag.Message = "Modifique esta plantilla para poner en marcha su aplicación ASP.NET MVC.";
+      
 
             return View();
         }
 
-        public ActionResult About()
-        {
-            ViewBag.Message = "Página de descripción de la aplicación.";
-
-            return View();
-        }
-
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Página de contacto.";
-
-            return View();
-        }
+    
     }
 }
--- 1 Fuentes/App_MEF_Expedientes/App_MEF_Expedientes/Controllers/LoginController.cs
+++ 1 Fuentes/App_MEF_Expedientes/App_MEF_Expedientes/Controllers/LoginController.cs
@@ -6,7 +6,8 @@ using System.Web.Mvc;
 using System.Diagnostics;
 using System.Configuration;
 using System.Security.Claims;
-using System.DirectoryServices; 
+using System.DirectoryServices;
+using MEF.Expedientes.Entity; 
 
 namespace App_MEF_Expedientes.Controllers
 {
@@ -19,23 +20,62 @@ namespace App_MEF_Expedientes.Controllers
         }
 
 
-        private bool AuthenticateUser(string user, string password)
+        public ActionResult Validar_usuario(string USUARIO, string CLAVE)
+        {
+            Cls_Ent_Auditoria auditoria = new Cls_Ent_Auditoria();
+            try
+            {
+                auditoria.Limpiar();
+                //bool autorizado = AuthenticateUser(USUARIO, CLAVE, ref auditoria);
+                //if (autorizado)
+                //{
+                //    //int ID_SISTEMA = int.Parse(ConfigurationManager.AppSettings["Codigo_Sistema"].ToString());
+                //    if (!auditoria.EJECUCION_PROCEDIMIENTO)
+                //    {
+                //        Recursos.Clases.Css_Log.Guardar(auditoria.ERROR_LOG);
+                //    }
+                //    else
+                //    {
+                //        if (!auditoria.RECHAZAR)
+                //        {
+                string ENCRIPTADO = Recursos.Clases.Css_Encriptar.Encrypt(USUARIO);
+                auditoria.OBJETO = ENCRIPTADO;
+                //        }
+                //    }
+                //}
+
+            }
+            catch (Exception ex)
+            {
+                auditoria.Error(ex);
+            }
+
+
+            return Json(auditoria, JsonRequestBehavior.AllowGet);
+        }
+
+
+
+        private bool AuthenticateUser(string user, string password, ref Cls_Ent_Auditoria auditoria)
         {
             bool respuesta = false; 
             string ldap = ConfigurationManager.AppSettings["LDAP"].ToString();
-            if (string.IsNullOrEmpty(ldap))
-            {
-                //.CreateLogger("LINK DE LDAP ESTA VACIO");
-            }
 
-   
             try
             {
+                if (string.IsNullOrEmpty(ldap))
+                {
+                    auditoria.Rechazar("Configuración active directory vacio.");
+                }
+                else { 
                 DirectoryEntry de = new DirectoryEntry("LDAP://" + ldap, user, password, System.DirectoryServices.AuthenticationTypes.Secure);
                 DirectorySearcher ds = new DirectorySearcher(de);
-                SearchResult result = ds.FindOne();
-                respuesta = true;
-                return respuesta;
+                    ds.Filter = "(SAMAccountName=" + user + ")";
+                    ds.PropertiesToLoad.Add("cn");
+                    SearchResult result = ds.FindOne();
+                 respuesta = true;
+                //return respuesta;
+               }
             }
             catch (Exception ex)
             {
@@ -43,12 +83,79 @@ namespace App_MEF_Expedientes.Controllers
                 mensaje += " si el error de codigo es : 525,52e,530,531,532,533,701,773,775";
                 mensaje += " la explicacion del error en el  siguiente link";
                 mensaje += " https://support.infrasightlabs.com/troubleshooting/common-error-codes-for-active-directory-authentication/";
-                Log.CreateLogger(mensaje);
-                respuesta.Success = false;
-                respuesta.Message = "Usuario / Contraseña  es incorrecta";
+                respuesta = false;         
+                auditoria.Rechazar("Usuario / Contraseña  es incorrecta.");
+                Recursos.Clases.Css_Log.Guardar(ex.Message); 
             }
             return respuesta;
         }
+
+
+
+
+        //public bool CheckUserInGroup(string group)
+
+        //{
+
+        //    string serverName = ConfigurationManager.AppSettings["ADServer"];
+
+        //    string userName = ConfigurationManager.AppSettings["ADUserName"];
+
+        //    string password = ConfigurationManager.AppSettings["ADPassword"];
+
+        //    bool result = false;
+
+        //    SecureString securePwd = null;
+
+        //    if (password != null)
+
+        //    {
+
+        //        securePwd = new SecureString();
+
+        //        foreach (char chr in password.ToCharArray())
+
+        //        {
+
+        //            securePwd.AppendChar(chr);
+
+        //        }
+
+        //    }
+
+        //    try
+
+        //    {
+
+        //        ActiveDirectory adConnectGroup = new ActiveDirectory(serverName, userName, securePwd);
+
+        //        SearchResultEntry groupResult = adConnectGroup.GetEntryByCommonName(group);
+
+        //        Group grp = new Group(adConnectGroup, groupResult);
+
+        //        SecurityPrincipal userPrincipal = grp.Members.Find(sp => sp.SAMAccountName.ToLower() == User.Identity.Name.ToLower());
+
+        //        if (userPrincipal != null)
+
+        //        {
+
+        //            result = true;
+
+        //        }
+
+        //    }
+
+        //    catch
+
+        //    {
+
+        //        result = false;
+
+        //    }
+
+        //    return result;
+
+        //}
 
     }
 }
