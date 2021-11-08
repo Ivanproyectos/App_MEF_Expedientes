5a0926e4b17c1b56f33da07187b3f3221070aa7e
Ivanproyectos <66085756+Ivanproyectos@users.noreply.github.com>
2021-11-08 10:32:28 -0500

    update

--- 1 Fuentes/App_MEF_Expedientes/App_MEF_Expedientes/Controllers/HomeController.cs
+++ 1 Fuentes/App_MEF_Expedientes/App_MEF_Expedientes/Controllers/HomeController.cs
@@ -33,19 +33,21 @@ namespace App_MEF_Expedientes.Controllers
                 Cls_Ent_Auditoria auditoria = new Cls_Ent_Auditoria();
                 Cls_Ent_Usuario MiUsuario = new Cls_Ent_Usuario(); 
                 MiUsuario = _cls_Serv_Login.Usuario( new Cls_Ent_Usuario { LOGIN_USUARIO = DESENCRIPTADO, ID_SISTEMA = ID_SISTEMA }, ref auditoria);
-                if (MiUsuario != null)
+                if (MiUsuario.ID_USUARIO  != 0)
                 {
                     @ViewBag.Usuario_Nombre = MiUsuario.NOMBRE_PERSONA;
                     @ViewBag.Usuario_Codigo = MiUsuario.LOGIN_USUARIO;
                     @ViewBag.Desc_Oficina = MiUsuario.NOMBRE_OFICINA;
                     @ViewBag.Id_Usuario = MiUsuario.ID_USUARIO;
                     ViewData["Seg_Perfiles"] = MiUsuario.Perfil;
+
                     Valido = true;
                 }
                 if (!Valido)
                 {
-
-                    return RedirectToAction("Index", "Login");
+                    //return RedirectToAction("Index", "Login");
+                    HttpContext.Response.Redirect(ConfigurationManager.AppSettings["NoAutorizado"].ToString());
+                    return null;
                 }
                 else
                 {
